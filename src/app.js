dd983e9bdb9d7a37337c4b343bb1f376ef043376
nathandacunha <nathandacunha233@gmail.com>
2026-07-16 20:04:05 -0300

    feat(cadastro): Criação da rota do tipo post para o cadastro do usuário

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cadastroRoutes from './routes/cadastro.js';
 
 // simula o __dirname em ESmodules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3300;
+const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// rota raiz que redireciona para o login
+// rota raiz que redireciona para o cadastro
 app.get('/', (req, res) => {
-    res.redirect('/src/public/pages/login.html');
+    res.redirect('/src/public/pages/cadastro.html');
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}/pages/cadastro.html`);
 });
+
+// usando a rota cadastro
+app.use(express.urlencoded({ extended: true}));
+app.use('/cadastro', cadastroRoutes);
---
+++ src/routes/cadastro.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const router = express.Router();
+
+// cria uma rota post 
+router.post('/', (req, res) => {
+    try {
+        console.log(req.body);
+        const { cpf, nome, senha } = req.body;
+        console.log("Cadastro recebido com sucesso!");
+        res.redirect("/pages/login.html");
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+export default router;
