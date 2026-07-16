0c4149c7568c174a927ead8d748581559fe2f7aa
nathandacunha <nathandacunha233@gmail.com>
2026-07-16 20:18:24 -0300

    fix(cadastro): correção do tipo de entrada no campo cpf

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { error } from 'node:console';
 const router = express.Router();
 
 // cria uma rota post 
@@ -7,10 +8,15 @@ router.post('/', (req, res) => {
         console.log(req.body);
         const { cpf, nome, senha } = req.body;
 
+        // verificando se os campos estão vazios
+        if(!cpf === "" || !nome === "" || !senha === "") {
+            return res.status(400).json({ erro: "Preenche todos os campos"});
+        }
+
         console.log("Cadastro recebido com sucesso!");
         res.redirect("/pages/login.html");
     } catch(error) {
-        console.log(error);
+        return res.status(500).json({ erro: "Erro interno no servidor. Tente mais tarde" });
     }
 });
 
