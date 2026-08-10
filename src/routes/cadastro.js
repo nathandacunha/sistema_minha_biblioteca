68103c80d51c9afbc21c5a197e7776fe77edebca
nathandacunha <nathandacunha233@gmail.com>
2026-08-10 17:37:52 -0300

    feat(cadastro):  adiciona a funcionalidade de gerar uma senha hash

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { error } from 'node:console';
 import conexao from '../models/db.js';
+import bcrypt from 'bcrypt';
 
 const cadastro = express.Router();
 
@@ -10,13 +11,14 @@ cadastro.post('/', (req, res) => {
         console.log(req.body);
         const { cpf, nome, senha } = req.body;
 
-        // salvando o cpf sem a formatação
-        // const cpfLimpo = cpf.replace(/\D\g, '');
-
+        
         // verificando se os campos estão vazios
         if(!cpf || !nome || !senha ) {
             return res.status(400).json({ erro: "Preenche todos os campos"});
         }
+
+        // salvando o cpf sem a formatação
+        // const cpfLimpo = cpf.replace(/\D\g, '');
 
         // verifica o tamanho dos digitos 
         if(cpf.length !== 14) {
@@ -29,9 +31,12 @@ cadastro.post('/', (req, res) => {
         }
 
         // verifica o tamanho da senha
-        if((senha.length < 8) || (senha.length > 16)) {
+        if((senha.length < 8) || (senha.length > 6)) {
             return res.status(400).json( { erro: "Sua senha deve ter entre 8 a 16 caracteres "});
         }
+
+        // gerando uma senha hash (10 é o numero de "rounds")
+        const senhaHash = await bcrypt.hash(senha, 10);
 
         // inserindo as informações do usuário no banco de dados
         const stmt = conexao.prepare("INSERT INTO usuarios (cpf, nome, senha) VALUES (?,?,?)");
