071fe8b20e0c8d781ee8d45d10a656c1541ea7c5
nathandacunha <nathandacunha233@gmail.com>
2026-08-12 05:30:00 -0300

    feat(cadastro): adiciona a validação de dados nos campos usando o express-validador

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,47 +1,44 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import { error } from 'node:console';
 import conexao from '../models/db.js';
 import bcrypt from 'bcrypt';
 
 const cadastro = express.Router();
+ 
+// criação das regras de validação uma por campo
+const regrasValidacoes = [ 
+    body('cpf') 
+        .trim()
+        .customSanitizer(valor => valor.replace(/\D/g, '')) // remove a pontuacao
+        .isLength( { min: 11, max: 11 } ).withMessage("CPF deve ter 11 digitos"),
+
+    body('nome')
+        .trim()
+        .isLength({ min: 6, max: 25 }).withMessage("Nome deve ter entre 6 a 25 caracteres"),
+
+    body('senha')
+        .isLength({ min: 8, max: 16 }).withMessage("Senha deve ter entre 8 a 16 caracteres"),
+];
 
 // cria uma rota post 
-cadastro.post('/', async (req, res) => {
+cadastro.post('/', regrasValidacoes, async (req, res) => {
     try {
-        console.log(req.body);
+        // verificacao se alguma regra falhou
+        const erros = validationResult(req);
+
+        if(!erros.isEmpty()) {
+            return res.status(400).json({ erros: erros.array() });
+        }
+
         const { cpf, nome, senha } = req.body;
-
-        
-        // verificando se os campos estão vazios
-        if(!cpf || !nome || !senha ) {
-            return res.status(400).json({ erro: "Preenche todos os campos"});
-        }
-
-        // salvando o cpf sem a formatação
-        const cpfLimpo = cpf.replace(/\D/g, '');
-
-        // verifica o tamanho dos digitos 
-        if(cpf.length !== 11) {
-            return res.status(400).json( { erro: "Seu cpf deve ter 14 digitos "})
-        }
-
-
-        // verifica o tamanho do nome
-        if((nome.length < 6) || (nome.length > 25)) {
-            return res.status(400).json( { erro: "Seu nome deve ter entre 6 a 25 caracteres "})
-        }
-
-        // verifica o tamanho da senha
-        if((senha.length < 8) || (senha.length > 16)) {
-            return res.status(400).json( { erro: "Sua senha deve ter entre 8 a 16 caracteres "});
-        }
 
         // gerando uma senha hash (10 é o numero de "rounds")
         const senhaHash = await bcrypt.hash(senha, 10);
 
         // inserindo as informações do usuário no banco de dados
         const stmt = conexao.prepare("INSERT INTO usuarios (cpf, nome, senha) VALUES (?,?,?)");
-        stmt.run(cpfLimpo, nome, senhaHash);
+        stmt.run(cpf, nome, senhaHash);
 
         console.log("Cadastro recebido com sucesso!");
         res.redirect("/pages/login.html");
