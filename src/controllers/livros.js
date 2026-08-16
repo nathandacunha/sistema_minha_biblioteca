b994bc9664c0388fbcb6144021f09305dd045aaf
nathandacunha <nathandacunha233@gmail.com>
2026-08-16 12:54:54 -0300

    feat(livros): criação da funcionalidade de adicionar livros

--- src/controllers/livros.js
+++ src/controllers/livros.js
@@ -1,46 +1,30 @@
 import express from 'express';
 import conexao from '../models/db.js';
+import { exigirLogin } from '../middlewares/autenticacao.js';
 
 const adicionarLivros = express.Router();
 
-adicionarLivros.post('/', (req, res) => {
-    try {
-        // protegendo a rota, usuário só logado pode cadastrar livros
-        if(!req.session.usuario) {
-            return res.status(401).json( { erro: "Você precisa estar logado" } );
-        }
+// Cria livro
+adicionarLivros.post('/', exigirLogin, (req, res, next) => {
+  try {
+    const { titulo, autor, genero, anoPublicacao, isbn } = req.body;
 
-        const { titulo, autor, genero, anoPublicacao, isbn } = req.body;
-
-        // verificando se titulo ou autor estão vazios
-        if((!titulo) || (!autor)) {
-            return res.status(400).json( { erro: "Titulo e ano são obrigatórios" } );
-        }
-
-        const stmt = conexao.prepare(`INSERT INTO livros (titulo, autor, genero, anoPublicacao, isbn, idUsuario) VALUES (?, ?, ?, ?, ?, ?)`);
-
-        stmt.run(titulo, autor, genero || null, anoPublicacao || null, isbn || null, req.session.usuario.id );
-
-        console.log(`Livro "${titulo}" cadastrado por ${req.session.usuario.nome}`);
-        res.redirect('/pages/index.html');
-    } catch(error) {
-        res.status(500).json({ erro: "Erro interno no servidor. Tente mais tarde"});
+    if (!titulo || !autor) {
+      return res.status(400).json({ erro: "Título e autor são obrigatórios" });
     }
+
+    const stmt = conexao.prepare(`
+      INSERT INTO livros (titulo, autor, genero, anoPublicacao, isbn, idUsuario)
+      VALUES (?, ?, ?, ?, ?, ?)
+    `);
+
+    stmt.run(titulo, autor, genero || null, anoPublicacao || null, isbn || null, req.session.usuario.id);
+
+    console.log(`Livro "${titulo}" cadastrado por ${req.session.usuario.nome}`);
+    res.redirect('/pages/index.html');
+  } catch (error) {
+    next(error);
+  }
 });
 
-adicionarLivros.get('/', (req, res) => {
-    try {
-        if (!req.session.usuario) {
-            return res.status(401).json({ erro: "Você precisa estar logado" });
-        }
-
-        const stmt = conexao.prepare('SELECT * FROM livros WHERE idUsuario = ?');
-        const livros = stmt.all(req.session.usuario.id);
-
-        res.json(livros);
-    } catch(error) {
-        console.log(error);
-        res.status(500).json({ erro: "Erro interno no servidor. Tente mais tarde" });
-    }
-});
 export default adicionarLivros;
