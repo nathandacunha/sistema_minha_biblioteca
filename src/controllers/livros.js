a94e4a041ef22bb61b38b989d480645d79747e3e
nathandacunha <nathandacunha233@gmail.com>
2026-08-16 13:04:06 -0300

    feat(livros): criação da funcionalidade de editar os livros

--- src/controllers/livros.js
+++ src/controllers/livros.js
@@ -44,4 +44,36 @@ adicionarLivros.get('/:id', (req, res, next) => {
     }
 });
 
+// editar um livro
+
+adicionarLivros.put('/:id', (req, res, next) => {
+    try {
+        const { titulo, autor, genero, anoPublicacao, isbn } = req.body;
+
+        // verifica se esta vazio nos campos de titulo e autor
+        if(!titulo || !autor) {
+            return res.status(400).json({erro: "Titulo e autor são obrigatórios"});
+        }
+
+        const stmt = conexao.prepare(`
+            UPDATE livros
+            SET titulo = ?, autor = ?, genero = ?, anoPublicacao = ?, isbn = ?
+            WHERE idLivro = ? AND idUsuario = ?
+        `);
+
+        const resultado = stmt.run(
+            titulo, autor, genero || null, anoPublicacao || null, isbn || null, 
+            req.params.id, req.session.usuario.id
+        );
+
+        if(resultado.changes === 0) {
+            return res.status(404).json({ erro: "Livro não encontrado" });
+        }
+
+        res.json({ mensagem: "Livro atualizado com sucesso" });
+    } catch(error) {
+        next(error);
+    }
+});
+
 export default adicionarLivros;
